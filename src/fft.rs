cd53007a9a8718ab52a787c09e5356b7206f7c03
Sean Friedowitz <sean@mozilla.ai>
2024-04-27 14:22:45 -0700

    got r2c fft working

--- src/fft.rs
+++ src/fft.rs
@@ -1,46 +1,82 @@
-use std::{borrow::Borrow, collections::HashMap};
-
 use ndarray::{ArrayD, IxDyn};
-use ndrustfft::{ndfft, ndifft, FftHandler, Normalization};
+use ndrustfft::{ndfft, ndfft_r2c, ndifft, ndifft_r2c, FftHandler, Normalization, R2cFftHandler};
 use num::complex::Complex64;
 
-pub struct FFTPlan {
-    handlers: HashMap<usize, FftHandler<f64>>,
-    work: ArrayD<Complex64>,
+/// Wrapper for real-to-complex FFTs over a multi-dimensional array.
+/// The real-to-complex transformation is performed over the last axis of the arrays.
+pub struct FFT {
+    dimensions: Vec<usize>,
+    r2c_handler: R2cFftHandler<f64>,
+    c2c_handlers: Vec<FftHandler<f64>>,
+    // Work buffers have half-size last dimension
+    work1: ArrayD<Complex64>,
+    work2: ArrayD<Complex64>,
 }
 
-impl FFTPlan {
+impl FFT {
     pub fn new(dimensions: &[usize], normalization: Option<Normalization<Complex64>>) -> Self {
-        let handlers = dimensions
+        let r2c_dim = dimensions.last().unwrap();
+        let r2c_handler = match &normalization {
+            Some(norm) => R2cFftHandler::<f64>::new(*r2c_dim).normalization(norm.clone()),
+            None => R2cFftHandler::<f64>::new(*r2c_dim),
+        };
+
+        let c2c_handlers = dimensions
             .iter()
-            .enumerate()
-            .map(|(i, d)| {
-                let h = match normalization.borrow() {
-                    Some(norm) => FftHandler::<f64>::new(*d).normalization(norm.clone()),
-                    None => FftHandler::<f64>::new(*d),
-                };
-                (i, h)
+            .take(dimensions.len() - 1)
+            .map(|d| match &normalization {
+                Some(norm) => FftHandler::<f64>::new(*d).normalization(norm.clone()),
+                None => FftHandler::<f64>::new(*d),
             })
             .collect();
 
-        let work = ArrayD::<Complex64>::zeros(IxDyn(dimensions));
+        let kdimensions = Self::complex_dimensions(dimensions);
+        let work1 = ArrayD::<Complex64>::zeros(IxDyn(&kdimensions));
+        let work2 = ArrayD::<Complex64>::zeros(IxDyn(&kdimensions));
 
-        Self { handlers, work }
-    }
-
-    pub fn forward(&mut self, x: &ArrayD<Complex64>, xhat: &mut ArrayD<Complex64>) {
-        self.work.assign(x);
-        for (axis, handler) in self.handlers.iter_mut() {
-            ndfft(&self.work, xhat, handler, *axis);
-            self.work.assign(xhat);
+        Self {
+            dimensions: dimensions.to_vec(),
+            r2c_handler,
+            c2c_handlers,
+            work1,
+            work2,
         }
     }
 
-    pub fn inverse(&mut self, xhat: &ArrayD<Complex64>, x: &mut ArrayD<Complex64>) {
-        self.work.assign(xhat);
-        for (axis, handler) in self.handlers.iter_mut() {
-            ndifft(&self.work, x, handler, *axis);
-            self.work.assign(x);
+    pub fn complex_dimensions(dimensions: &[usize]) -> Vec<usize> {
+        let mut kdims = dimensions.to_vec();
+        let r2c_dim = kdims.last_mut().unwrap();
+        *r2c_dim = *r2c_dim / 2 + 1;
+        kdims
+    }
+
+    pub fn n_dimensions(&self) -> usize {
+        self.dimensions.len()
+    }
+
+    pub fn forward(&mut self, input: &ArrayD<f64>, output: &mut ArrayD<Complex64>) {
+        // Transform the real -> complex dimension first
+        let r2c_axis = self.n_dimensions() - 1;
+        ndfft_r2c(input, output, &mut self.r2c_handler, r2c_axis);
+
+        // Transform the remaining complex -> complex axes
+        self.work1.assign(output);
+        for (c2c_axis, c2c_handler) in self.c2c_handlers.iter_mut().enumerate() {
+            ndfft(&self.work1, output, c2c_handler, c2c_axis);
+            self.work1.assign(output);
         }
+    }
+
+    pub fn inverse(&mut self, input: &ArrayD<Complex64>, output: &mut ArrayD<f64>) {
+        // Transform the complex -> complex axes first
+        self.work1.assign(input);
+        for (c2c_axis, c2c_handler) in self.c2c_handlers.iter_mut().enumerate() {
+            ndifft(&self.work1, &mut self.work2, c2c_handler, c2c_axis);
+            self.work1.assign(&self.work2);
+        }
+
+        // Transform the accumulated work into the output array
+        let r2c_axis = self.n_dimensions() - 1;
+        ndifft_r2c(&self.work2, output, &mut self.r2c_handler, r2c_axis);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 use ndarray::{ArrayD, IxDyn};
 use num::complex::Complex64;
-
-use rfts::fft::FFTPlan;
+use rfts::fft::FFT;
 
 pub fn main() {
-    let mut fft = FFTPlan::new(&[4, 4], None);
+    let mut fft = FFT::new(&[4, 4, 4, 4], None);
 
-    let (nx, ny) = (4, 4);
-    let mut data = ArrayD::<Complex64>::zeros(IxDyn(&[nx, ny]));
-    let mut output = ArrayD::<Complex64>::zeros(IxDyn(&[nx, ny]));
-    for (i, v) in data.iter_mut().enumerate() {
-        *v = Complex64::new(i as f64, 0.0);
+    let (nx, ny, nz, nw) = (4, 4, 4, 4);
+    let mut input = ArrayD::<f64>::zeros(IxDyn(&[nx, ny, nz, nw]));
+    for (i, v) in input.iter_mut().enumerate() {
+        *v = i as f64;
     }
 
-    fft.forward(&data, &mut output);
-    fft.inverse(&output, &mut data);
+    let mut output = ArrayD::<Complex64>::zeros(IxDyn(&[nx, ny, nz, nw / 2 + 1]));
 
-    println!("{}", data);
+    fft.forward(&input, &mut output);
+    fft.inverse(&output, &mut input);
+
+    println!("{}", input);
 }
